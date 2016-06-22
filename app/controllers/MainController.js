7d5ab20963210f9fd4ab208f0a9aee9ee074bbaf
Jason Weakley <jason.b.weakley@gmail.com>
2016-06-22 18:51:00 -0500

    able to edit and delete products

--- app/controllers/MainController.js
+++ app/controllers/MainController.js
@@ -3,24 +3,44 @@
 MediaBay.controller('MainController', [
 	'$http', 
 	'$scope',
+	'$location',
 
-	function ($http, $scope) {
+	function ($http, $scope, $location) {
 		console.log("MainController is working");
 
-		$scope.products = {};
+		$scope.products = [];
 
 		// What is happening with "$scope.'something'"? 
 		// Is it assigning value of scope to the thing after the dot?
 		// I would expect "$scope = products" if that were the case.
 
 		$http
-			.get('http://localhost:5000/api/Product')
-			.success(p => $scope.products = p);
+			.get(`http://localhost:5000/api/Product`)
+			.success(prod => $scope.products = prod);
 			/// These do not work, but keeping here for learning purposes:
 			// .success(p => $scope.$parent.products = p);
 			// .success(p => $scope.$parent.p[0]);
 			// .then(console.log($scope.products));
 
+
+		// Create new products
+		$scope.createView = function () {
+			$location.path("/createproduct")
+		};
+
+		// Get product details
+		$scope.detailView = function (id) {
+			$http({
+				url:`http://localhost:5000/api/Product/${id}`,
+				method:'GET'
+			})
+			.success( function(prod) {
+				$scope.$parent.prod = prod;
+			})
+			.then( function() {
+				$location.path("/productdetails")
+			})
+		};
 	}
 
 ]);
--- app/controllers/NewProductController.js
+++ app/controllers/NewProductController.js
@@ -3,22 +3,97 @@
 MediaBay.controller('NewProductController', [
 	'$http',
 	'$scope',
+	'$location',
 
-	function ($http, $scope) {
+	function ($http, $scope, $location) {
 
 		$scope.product = [];
 
+		// Get products and scope the data equal to the var "prod"
+		$http
+			.get(`http://localhost:5000/api/Product`)
+			.success(prod => $scope.product = prod);
+
+		console.log("NewProductController is working");
+
+		// Create new products
 		$scope.createProduct = function () {
+			console.log("createProduct");
 			$http({
-				url:'http://localhost:5000/api/Product',
+				url: `http://localhost:5000/api/Product`,
 				method:'POST',
 				data: JSON.stringify({
 					name: $scope.product.name,
 					description: $scope.product.description,
-					price: $scope.product.price
+					unitprice: $scope.product.price
 				})
 			})
 			.success(newProduct => console.log("201 Created", newProduct))
 		};
+
+		// Get product details
+		$scope.detailProduct = function (id) {
+			console.log("detailProduct");
+			$http({
+				url:`http://localhost:5000/api/Product/${id}`,
+				method:'GET'
+			})
+			.success( function(prod) {
+				$scope.$parent.prod = prod;
+			})
+			.then( function() {
+				$location.path("/productdetails")
+			})
+		};
+
+		// Edit existing products
+		$scope.editProduct = function (id) {
+			$http({
+				url: `http://localhost:5000/api/Product/${id}`,
+				method: 'GET'
+			})
+			.success( function(prod) {
+				$scope.$parent.prod = prod;
+			})
+			.then(function() {
+				$location.path("/editproduct");
+			})
+		};
+
+		// Save changes to edited product
+		$scope.saveProduct = function (id) {
+			console.log(id);
+			$http({
+				url: `http://localhost:5000/api/Product/${id}`,
+				method: 'PUT',
+				data: JSON.stringify({
+					ProductId: $scope.prod.ProductId,
+					Name: $scope.prod.Name,
+					Description: $scope.prod.Description,
+					UnitPrice: $scope.prod.UnitPrice
+				})
+			})
+			.success( function(prod) {
+				console.log(prod);
+				$scope.$parent.prod = prod;
+			})
+			.then( function() {
+				$location.path("/")
+			})
+		};
+
+		$scope.deleteProduct = function(id) {
+			$http({
+				url: `http://localhost:5000/api/Product/${id}`,
+				method: 'DELETE'
+			})
+			.success( function(prod) {
+				$scope.$parent.prod = prod;
+			})
+			.then( function() {
+				$location.path("/")
+			})
+		};
+
 	}
 ]);
